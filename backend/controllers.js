9c017abf99403718f21d0638773be70c9fedd238
hollowlamp <2504384479@qq.com>
2024-09-20 18:44:24 +0800

    update

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,11 +1,26 @@
 import neo4j from 'neo4j-driver';
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', '12345678'));
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'srtp'));
 
-export const getAllStation = async (req, res) => {
+export const getAllCities = async (req, res) => {
   const session = driver.session();
   try {
-    const result = await session.run('MATCH (s:station) return s.nodeId');
+    const result = await session.run('MATCH (c:city) RETURN c.nodeId');
+    const cityNames = result.records.map(record => record.get(0));
+    res.send(cityNames);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching cities');
+  } finally {
+    await session.close();
+  }
+}
+
+export const getAllStation = async (req, res) => {
+  const { city } = req.params;
+  const session = driver.session();
+  try {
+    const result = await session.run('MATCH (s:station)-[:locateAtCity]->(c:city {nodeId: $city}) RETURN s.nodeId', { city });
     const stationNames = result.records.map(record => record.get(0));
     res.send(stationNames);
   } catch (error) {
@@ -17,13 +32,14 @@ export const getAllStation = async (req, res) => {
 }
 
 export const getPolyonOfStation = async (req, res) => {
-  const { station, polygon } = req.params;
+  const { city, station, polygon } = req.params;
   const session = driver.session();
   try {
     const result = await session.run(`
-      MATCH (s:station {nodeId: $station})-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})
+      MATCH (s:station {nodeId: $station})-[:locateAtCity]->(c:city {nodeId: $city})
+      MATCH (s)-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})
       RETURN p.location as location
-    `, { station, polygon });
+    `, { city, station, polygon });
     const location = result.records.map(record => record.get(0));
     res.send(location);
   } catch (error) {
@@ -35,13 +51,14 @@ export const getPolyonOfStation = async (req, res) => {
 }
 
 export const getBlockInPolygon = async (req, res) => {
-  const { station, polygon } = req.params;
+  const { city, station, polygon } = req.params;
   const session = driver.session();
   try {
     const result = await session.run(`
-      MATCH (s:station {nodeId: $station})-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block)
+      MATCH (s:station {nodeId: $station})-[:locateAtCity]->(c:city {nodeId: $city})
+      MATCH (s)-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block)
       RETURN b.nodeId as blockName
-    `, { station, polygon });
+    `, { city, station, polygon });
     const blockNames = result.records.map(record => record.get('blockName'));
     res.send(blockNames);
   } catch (error) {
@@ -53,13 +70,14 @@ export const getBlockInPolygon = async (req, res) => {
 };
 
 export const getPropertyOfAllBlockInPolygon = async (req, res) => {
-  const { station, polygon, property } = req.params;
+  const { city, station, polygon, property } = req.params;
   const session = driver.session();
   try {
     const result = await session.run(`
-      MATCH (s:station {nodeId: $station})-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block)
+      MATCH (s:station {nodeId: $station})-[:locateAtCity]->(c:city {nodeId: $city})
+      MATCH (s)-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block)
       RETURN b[$property] as propertyValue
-    `, { station, polygon, property });
+    `, { city, station, polygon, property });
     const propertyValues = result.records.map(record => record.get('propertyValue'));
     res.send(propertyValues);
   } catch (error) {
@@ -71,13 +89,14 @@ export const getPropertyOfAllBlockInPolygon = async (req, res) => {
 }
 
 export const getPropertyOfBlockInPolygon = async (req, res) => {
-  const { station, polygon, block, property } = req.params;
+  const { city, station, polygon, block, property } = req.params;
   const session = driver.session();
   try {
     const result = await session.run(`
-      MATCH (s:station {nodeId: $station})-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block {nodeId: $block})
+      MATCH (s:station {nodeId: $station})-[:locateAtCity]->(c:city {nodeId: $city})
+      MATCH (s)-[:haveIso]->(p:staion_isochrone {nodeId: $polygon})-[:includeBlock]->(b:block {nodeId: $block})
       RETURN b[$property] as propertyValue
-    `, { station, polygon, block, property });
+    `, { city, station, polygon, block, property });
     const propertyValue = result.records.map(record => record.get('propertyValue'));
     res.send(propertyValue);
   } catch (error) {
@@ -89,13 +108,13 @@ export const getPropertyOfBlockInPolygon = async (req, res) => {
 }
 
 export const getEntranceLocations = async (req, res) => {
-  const { station } = req.params;
+  const { city, station } = req.params;
   const session = driver.session();
   try {
     const result = await session.run(`
-      MATCH (s:station {nodeId: $station})
+      MATCH (s:station {nodeId: $station})-[:locateAtCity]->(c:city {nodeId: $city})
       RETURN s.entrance_locations as entranceLocations
-    `, { station });
+    `, { city, station });
     const entranceLocations = result.records.map(record => record.get('entranceLocations'));
     res.send(entranceLocations);
   } catch (error) {
--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { getAllStation, getPolyonOfStation, getBlockInPolygon, getPropertyOfAllBlockInPolygon, getPropertyOfBlockInPolygon, getEntranceLocations } from './controllers.js';
+import { getAllStation, getPolyonOfStation, getBlockInPolygon, getPropertyOfAllBlockInPolygon, getPropertyOfBlockInPolygon, getEntranceLocations, getAllCities } from './controllers.js';
 
 const router = express.Router();
+
+// 获取所有城市列表
+router.get('/query/cities', getAllCities);
 
 // 获取所有站点名称
 router.get('/query/:city/station', getAllStation);
@@ -13,15 +16,12 @@ router.get('/query/:city/:station/entrance_locations', getEntranceLocations);
 router.get('/query/:city/:station/:polygon', getPolyonOfStation);
 
 // 获取指定站点下的指定轮廓下的所有地块的名称
-router.get('/query/:city/:station/:polygon/block/', getBlockInPolygon);
+router.get('/query/:city/:station/:polygon/block', getBlockInPolygon);
 
 // 获取指定站点下的指定轮廓下的所有地块的指定属性
 router.get('/query/:city/:station/:polygon/block/:property', getPropertyOfAllBlockInPolygon);
 
 // 获取指定站点下的指定轮廓下的指定地块的指定属性
 router.get('/query/:city/:station/:polygon/:block/:property', getPropertyOfBlockInPolygon);
-
-
-
 
 export default router;
